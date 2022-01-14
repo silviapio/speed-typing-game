dd4251ec016282801640a9156937344c5d258c80
Silvia <silvia.piovesan@gmail.com>
2022-01-14 15:41:55 +0100

    App setup-working text area

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
+import {useState} from "react";
 import './App.css';
 
 function App() {
+  const [textInput, setTextInput] = useState("");
+
+  const handleInput = event => {
+    setTextInput(event.target.value);
+  }
+
+  const getWordsCount = str => {
+    const wordsArray = str.split(" ");
+    return wordsArray.filter(item => item !== "").length;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <div>
+          <h1>How fast do you type?</h1>
+          <textarea 
+            value={textInput} 
+            onChange={handleInput} 
+          />
+          <h4>Time reminaing: ???</h4>
+          <button onClick={() => console.log(getWordsCount(textInput))}>Start</button>
+          <h1>Word count: ???</h1>
+      </div>
+  )
 }
 
-export default App;
+export default App
