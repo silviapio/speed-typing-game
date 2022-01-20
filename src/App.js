1d3a6a0d94bc3c0340df8cb84acd778b6483c3ac
Silvia <silvia.piovesan@gmail.com>
2022-01-20 11:55:17 +0100

    feat: start button

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import {useEffect, useState} from "react";
 import './App.css';
 
 function App() {
+  const [isGameRunning, setIsGameRunning] = useState(false);
   const [textInput, setTextInput] = useState("");
   const [secondsLeft, setSecondsLeft] = useState(10);
 
@@ -9,23 +10,24 @@ function App() {
     setTextInput(event.target.value);
   }
 
-  const getWordsCount = str => {
-    const wordsArray = str.split(" ");
-    return wordsArray.filter(item => item !== "").length;
-  }
+  // const getWordsCount = str => {
+  //   const wordsArray = str.split(" ");
+  //   return wordsArray.filter(item => item !== "").length;
+  // }
 
   useEffect(() => {
     let timer;
-    if (secondsLeft > 0) {
+    if (secondsLeft > 0 && isGameRunning) {
       timer = setTimeout(() => {
         setSecondsLeft(prevCount => prevCount - 1)
       }, 1000)
-    } else {
+    } else if (secondsLeft === 0) {
+      setIsGameRunning(false);
       return function() {
         clearTimeout(timer);
       }
     }
-  }, [secondsLeft])
+  }, [secondsLeft, isGameRunning]);
 
   return (
       <div>
@@ -34,8 +36,8 @@ function App() {
             value={textInput} 
             onChange={handleInput} 
           />
-          <h4>Time reminaing: {secondsLeft}</h4>
-          <button onClick={() => console.log(getWordsCount(textInput))}>Start</button>
+          <h4>Time remaining: {secondsLeft}</h4>
+          <button onClick={() => setIsGameRunning(true)}>Start</button>
           <h1>Word count: ???</h1>
       </div>
   )
