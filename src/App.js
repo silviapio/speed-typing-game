e704ab2ef727760a25fc5d911114a205b15b4411
Silvia <silvia.piovesan@gmail.com>
2022-01-20 10:27:23 +0100

    feat:countdown

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-import {useState} from "react";
+import {useEffect, useState} from "react";
 import './App.css';
 
 function App() {
   const [textInput, setTextInput] = useState("");
+  const [secondsLeft, setSecondsLeft] = useState(10);
 
   const handleInput = event => {
     setTextInput(event.target.value);
@@ -13,6 +14,19 @@ function App() {
     return wordsArray.filter(item => item !== "").length;
   }
 
+  useEffect(() => {
+    let timer;
+    if (secondsLeft > 0) {
+      timer = setTimeout(() => {
+        setSecondsLeft(prevCount => prevCount - 1)
+      }, 1000)
+    } else {
+      return function() {
+        clearTimeout(timer);
+      }
+    }
+  }, [secondsLeft])
+
   return (
       <div>
           <h1>How fast do you type?</h1>
@@ -20,7 +34,7 @@ function App() {
             value={textInput} 
             onChange={handleInput} 
           />
-          <h4>Time reminaing: ???</h4>
+          <h4>Time reminaing: {secondsLeft}</h4>
           <button onClick={() => console.log(getWordsCount(textInput))}>Start</button>
           <h1>Word count: ???</h1>
       </div>
